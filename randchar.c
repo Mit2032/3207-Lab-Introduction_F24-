252a4902ea8dd6e90829fcda9717894fa96503d8
Mit2032 <tul64964@temple.edu>
2024-09-01 22:09:30 -0400

    Added the random character generator

--- randchar.c
+++ randchar.c
@@ -1,3 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include "random.h"
+
+char randchar(void){
+    
+    //define the range
+    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int charset_length = sizeof(charset) - 1; //exlude the null terminator
+
+    int random_index = rand() % charset_length;
+
+    return charset[random_index];
+
+
+}
